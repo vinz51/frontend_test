c857a047071b2bb4dc107189d279f2fe9feef181
Vincent <vincent.benoit@weezevent.com>
2020-07-20 14:52:12 +0200

    Add global styles

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+export default {
+  DarkBlue: "#34495e",
+  DarkerBlue: "#2c3e50",
+  LightBlue: "#3498db",
+  LighGrey: "#ecf0f1",
+  Green: "#2ecc71",
+  Grey: "#bdc3c7",
+  Red: "#e74c3c",
+  White: "#fff",
+};
